789c60d90831420ea1849dea7dd3e00246bb2df3
rohitkhushdil1444-hue <rohitkhushdil1444@gmail.com>
2026-01-04 15:54:50 +0530

    First program commit

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -1,7 +1,26 @@
 package ex_01_Java_Basics;
 
+import java.sql.SQLOutput;
+
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello" );
-    }
-}
+
+        int a=10;
+        int b=20;
+
+            System.out.println(a+b);
+            result();
+
+        }
+
+        public static void result(){
+        System.out.println("ABC");
+        }
+
+
+
+        }
+
+
+
