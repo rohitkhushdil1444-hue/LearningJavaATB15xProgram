85bcb4dd301ad6dc5b8bc676bd54a4c5400d52f4
rohitkhushdil1444-hue <rohitkhushdil1444@gmail.com>
2026-02-11 16:15:02 +0530

    ArrayList-Program

---
+++ src/ex_01_Java_Basics/Arraylist.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basics;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Arraylist {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList();
+
+        list.add("Rohit");
+        list.add("Mohit");
+        list.add("Rishabh");
+
+        System.out.println(list);
+
+
+    }
+}
